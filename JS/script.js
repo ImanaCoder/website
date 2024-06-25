d5cb7b0fb9c77c0f642739d72f9d37ee5f334c27
unknown <imanacoder@gmail.com>
2024-06-25 14:08:12 +0545

    commit

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+// scroll to top (#to home) btn
+
+let scrollBtn = document.getElementById("to-home");
+
+window.addEventListener("scroll", ()=>{
+    if (window.scrollY > 0) {
+        scrollBtn.style.visibility = "visible";
+    } else {
+        scrollBtn.style.visibility = "hidden";
+    }
+
+});
